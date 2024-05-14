377c2fd6e3d2bc1381b189e98c43faec3ca5438e
KotletasGT <167352451+KotletasGT@users.noreply.github.com>
2024-05-14 21:19:04 +0300

    v0.7

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -7,41 +8,46 @@ public class Main extends Frame {
     public static void main(String[] args) {
         new Main();
     }
-    int rows = 8; // X 
-    int cols = 8; // Y
+    int rows = 8; //X   ////Galimai useless, galima daryti kad visada kvadratas bet taip labiau cool
+    int cols = 8; //Y
     int tileSize = 50; // Size of each tile itself
     boolean[][] revealed; // Tracks if tile is revealed
     int bombCount = 10;
-    boolean[][] bombs; // Locations of bombs
+    boolean[][] bombs; //Locations of bombs
     boolean gameOver;
     Image bombImage; //.png of da boom boom
+    Image markImage; //Image for when right-clicked
 
     public Main() {
-        bombImage = Toolkit.getDefaultToolkit().getImage("C:\\PATH\\TO\\REPO\\Fugnus1.png"); // Load the bomb image, for now this has to be an ABSOLUTE path, will be fixed next stage
-        // bombImage = Toolkit.getDefaultToolkit().getImage("Fugnus1.png"); // Load the bomb image
+        bombImage = Toolkit.getDefaultToolkit().getImage("C:\\Users\\ignas\\Desktop\\Fugnus1.png"); // Load the bomb image
+        //bombImage = Toolkit.getDefaultToolkit().getImage("Fugnus1.png"); // Load the bomb image
+        markImage = Toolkit.getDefaultToolkit().getImage("C:\\Users\\ignas\\Desktop\\313.jpg");
 
         setTitle("Fugusweeper");
-        setSize(cols * tileSize + 20, rows * tileSize + 40); // Also makes it slightly further away from windows border
+        setSize(cols * tileSize + 20, rows * tileSize + 100); //Adds some kad netouchintu borderio + restartui
         setVisible(true);
 
         revealed = new boolean[rows][cols];
         bombs = new boolean[rows][cols];
         gameOver = false;
 
-        Random rand = new Random();
-        int placedBombs = 0;
+        placeBombs();
 
-        while (placedBombs < bombCount) {
-            int x = rand.nextInt(rows);
-            int y = rand.nextInt(cols);
+        Button restartButton = new Button("Restart"); //NEW RESTART BUTTON
+        restartButton.setBounds(10, rows * tileSize + 50, 80, 30); //padedamas apacioj
+        restartButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                revealed = new boolean[rows][cols];
+                bombs = new boolean[rows][cols];
+                gameOver = false;
 
-            if (!bombs[x][y]) { // So that there are no duplicates
-                bombs[x][y] = true;
-                placedBombs++;
+                placeBombs();
+                repaint();
             }
-        }
+        });
+        //add(restartButton); //SWINGAS YRA HORSHESHIT SU POSITIONINGU SETBOUNDS NUSTATYTA BET BUTTON VISTIEK PER VISA EKRANA JEIGU add
 
-        addWindowListener(new WindowAdapter() { // Copied
+        addWindowListener(new WindowAdapter() { //copied
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
@@ -50,28 +56,47 @@ public class Main extends Frame {
         addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent press) {
                 if (gameOver) {
-                    return; // Does not let player click anymore if game is over
+                    return; //Does not let player click anymore if game is over
                 }
                 int row = press.getY() / tileSize;
                 int col = press.getX() / tileSize;
                 if (row >= 0 && row < rows && col >= 0 && col < cols) {
+                    if (press.getButton() == MouseEvent.BUTTON1) {
                     if (bombs[row][col]) {
                         gameOver = true;
                         for (int temprow = 0; temprow < rows; temprow++) {
                             for (int tempcol = 0; tempcol < cols; tempcol++) {
-                                if (bombs[temprow][tempcol]) { // Reveals all bombs
+                                if (bombs[temprow][tempcol]) { //Reveals all bombs. Kind of sviestas sviestuotas situation but whatever
                                     revealed[temprow][tempcol] = true;
                                 }
                             }
                         }
-                    }
-                    else {
+                    } else {
                         revealed[row][col] = true;
                     }
-                    repaint(); // Calls paint
+                    repaint(); //Kviecia paint su braindead awt logika kur g yra simboliskai revealed masyvas
                 }
-            }
+            } else if(press.getButton() == MouseEvent.BUTTON3) { //Desinys mouse
+                    //TODO
+                    repaint();
+                }
+        }
         });
+    }
+
+    private void placeBombs() {
+        Random rand = new Random();
+        int placedBombs = 0;
+
+        while (placedBombs < bombCount) {
+            int x = rand.nextInt(rows);
+            int y = rand.nextInt(cols);
+
+            if (!bombs[x][y]) { // Ensure no duplicates
+                bombs[x][y] = true;
+                placedBombs++;
+            }
+        }
     }
 
     @Override
@@ -80,33 +105,51 @@ public class Main extends Frame {
         g.setColor(Color.BLACK);
         g.drawRect(10, 30, cols * tileSize, rows * tileSize);
 
-        Font font = new Font("Comic Sans", Font.BOLD, 20); // Unfortunately, numbers don't work yet :(
+        Font font = new Font("Comic Sans", Font.BOLD, 20); //no FUCKING CLUE KODEL NEVEIKIA, nebent reikia JFrame arba JPanel susikurt??
         g.setFont(font);
         FontMetrics metrics = g.getFontMetrics(font);
         int stringWidth = metrics.stringWidth("Fugusweeper");
-        g.drawString("Fugusweeper", getWidth() / 2 - stringWidth / 2, 20); // This should write the title in the middle
+        g.drawString("Fugusweeper", getWidth() / 2 - stringWidth / 2, 20); //Turetu per viduri borderio rasyti pavadinima
 */
+        //MAIN UZPILDYMAS
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 int x = col * tileSize + 10;
                 int y = row * tileSize + 30;
 
                 if (revealed[row][col]) {
-                    g.setColor(Color.BLUE);                 // Tile
+                    int neighborBombs = countNearBombs(row, col);
+                    g.setColor(Color.BLUE);                 //TILE
                     g.fillRect(x, y, tileSize, tileSize);
-                    g.setColor(Color.BLACK);                // Border
+                    g.setColor(Color.BLACK);                //BORDERIS
                     g.drawRect(x, y, tileSize, tileSize);
-                    if (bombs[row][col]) { // If tile has bomb show it
+                    if (bombs[row][col]) { //If tile has bomb show it
                         g.drawImage(bombImage, x, y, tileSize, tileSize, this);
+                    } else if (neighborBombs > 0) {
+                        g.drawString(Integer.toString(neighborBombs), x + tileSize / 2, y + tileSize / 2);
                     }
-                } else {                                    // Fill with gray if not revealed yet
+                } else {                                    //JEI DAR NIEKO NERA, UZPILDO GRAY (pradzioje)
                     g.setColor(Color.DARK_GRAY);
                     g.fillRect(x, y, tileSize, tileSize);
                     g.setColor(Color.BLACK);
                     g.drawRect(x, y, tileSize, tileSize);
-                    // g.drawImage(bombImage, x, y, tileSize, tileSize, this); // Test statement
+                    //g.drawImage(bombImage, x, y, tileSize, tileSize, this); //TESTAVIMUI AR IS VIS KAZKA DRAWINA IS VIS
                 }
             }
         }
     }
+
+private int countNearBombs(int row, int col) {
+    int count = 0;
+    for (int dr = -1; dr <= 1; dr++) {
+        for (int dc = -1; dc <= 1; dc++) {
+            int r = row + dr;
+            int c = col + dc;
+            if (r >= 0 && r < rows && c >= 0 && c < cols && bombs[r][c]) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
 }
